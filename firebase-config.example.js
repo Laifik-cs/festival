f592811af6f95bec4f6a6c52599da41aa5b5981a
Laifik-cs <laifik1200@mail.ru>
2025-09-14 17:02:17 +0700

    add config

---
+++ firebase-config.example.js
@@ -0,0 +1,10 @@
+// firebase-config.example.js
+// Скопируйте этот файл в firebase-config.js и заполните своими данными
+const firebaseConfig = {
+    apiKey: "your_api_key_here", // Заглушка
+    authDomain: "your_project.firebaseapp.com", // Заглушка
+    projectId: "your_project_id", // Заглушка
+    storageBucket: "your_project.appspot.com", // Заглушка
+    messagingSenderId: "123456789", // Заглушка
+    appId: "1:123456789:web:abcdef123456" // Заглушка
+};
