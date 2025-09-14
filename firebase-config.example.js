604273c2ad7d8ffaa282890717d4c7edc4b27bbe
Кирилл Кольцов <laifik1200@mail.ru>
2025-09-14 17:52:25 +0700

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.example.js
-const firebaseConfig = {
-    apiKey: "your_api_key_here", // Заглушка
-    authDomain: "your_project.firebaseapp.com", // Заглушка
-    projectId: "your_project_id", // Заглушка
-    storageBucket: "your_project.appspot.com", // Заглушка
-    messagingSenderId: "123456789", // Заглушка
-    appId: "1:123456789:web:abcdef123456" // Заглушка
-};
