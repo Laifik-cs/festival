3fd16353c61d7a345f6445d75d1cfd4b734e8215
Кирилл Кольцов <laifik1200@mail.ru>
2025-09-14 17:47:50 +0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// firebase-config.js
-const firebaseConfig = {
-    apiKey: "AIzaSyChW1XWwnkfJoB-id7ZEXStMr7lXOMfwt4",
-    authDomain: "votes-tomsk.firebaseapp.com",
-    projectId: "votes-tomsk",
-    storageBucket: "votes-tomsk.firebasestorage.app",
-    messagingSenderId: "370426111734",
-    appId: "1:370426111734:web:96c5187162d7f0fedd2ed7"
-};
